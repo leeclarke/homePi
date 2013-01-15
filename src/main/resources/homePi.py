8f67eb9b3596ad63d300d17f2c7bc225305839e0
Lee Clarke <lees2bytes@gmail.com>
2013-01-15 00:02:01 -0500

    Update to build 4

--- src/main/resources/homePi.py
+++ src/main/resources/homePi.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+#TODO: ADD TO CronTab
+
 """
   HomePi -  keeping in touch with Home.
   
@@ -14,19 +16,21 @@
 """
 
 from hpcommon import *
-import urllib2, os, time, json
+import getopt,sys,subprocess,urllib2, os, time, json
 from Configuration import Configuration
 from subprocess import Popen
+
+helpInfo = 'homePi.py -a <appName> -m <logmessage> | -f <logFilePath>  \nOr call no-op for standard function.'
 
 #Read ini file
 config = Configuration('homePi.ini')
 
 def getVersion():
-  return 3
+  return 4
 
 #Currently Downloads updates only for HomePi but as this script evolves updates for supported apps will be included as well.
 def downloadUpdates():
-  #Determiine if it's time for an update check  
+  #Determine if it's time for an update check  
   updateTime = float(config.main.lastupdate) + (float(config.main.updatefreqhrs) * 3600)
   print "curTime= %f > lastUpdate: %f" % (time.time(), updateTime)
   if time.time() > updateTime:
@@ -56,9 +60,60 @@ def downloadUpdates():
     #Set updateTime
     config.setValue("main","lastUpdate",str(time.time()))
     print 'HomePi Ready for update.'
+
+# Execute commands requested by args provided.
+def processCommands(argv):
+  try:
+    opts, args = getopt.getopt(argv,"ha:m:f:")
+  except getopt.GetoptError:
+    print helpInfo
+    sys.exit(2)
+      
+  appName, logmessage,filePath = parseArgs(opts)
+  
+  #DEBUGING print "appName= %s , logmessage= %s filePath= %s"  % (appName,logmessage,filePath)
+
+#TODO: Finish by adding the logging API calls. and HomePi logging
+  if appName != None:
+    if logmessage != None:
+      # call log msg
+      print logmessage
+    elif filePath != None:
+      p = Popen(['tail', '-n40', filePath], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      out, err = p.communicate()
+      #call log message
+      print out
+    else:
+      print helpInfo
+    sys.exit(2)
     
-def main():
+  print 'No opts'
+
+#parses any provides arguments and returns values if present
+def parseArgs(opts):
+  appName = None
+  logmessage = None
+  filePath = None
+  
+  for opt, arg in opts:
+    if opt == '-h':
+      print helpInfo 
+      sys.exit()
+    elif opt in ("-a"):
+      appName = arg
+    elif opt in ("-m"):
+      logmessage = arg
+    elif opt in ("-f"):
+      filePath = arg
+   
+  return appName, logmessage,filePath  
+    
+### Main    
+def main(argv):
   print 'running... HomePi build: %d' % getVersion()
+  #Special processing of command args if present
+  processCommands(argv)
+  
   #Check for HomePi updates.
   downloadUpdates()
   
@@ -68,4 +123,4 @@ def main():
   exit("HomePi done.")
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
