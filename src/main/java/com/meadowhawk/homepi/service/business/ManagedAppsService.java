c4b502ee39aa654571d2cdd9f6114e5028f50fc7
leeclarke <lees2bytes@gmail.com>
2013-04-15 21:29:12 -0400

    Added ManagedAppService to beans.

--- src/main/java/com/meadowhawk/homepi/service/business/ManagedAppsService.java
+++ src/main/java/com/meadowhawk/homepi/service/business/ManagedAppsService.java
@@ -3,6 +3,7 @@ package com.meadowhawk.homepi.service.business;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.meadowhawk.homepi.dao.ManagedAppDAO;
 import com.meadowhawk.homepi.model.ManagedApp;
@@ -11,6 +12,7 @@ import com.meadowhawk.homepi.model.ManagedApp;
  * Business logic specific to the ManagedApps for a given Pi and/or user.
  * @author lee
  */
+@Component
 public class ManagedAppsService {
 
 	@Autowired
