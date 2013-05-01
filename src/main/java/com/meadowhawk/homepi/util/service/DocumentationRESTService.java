6aba75f01b70c2927541b12a2aac0905b01ef44e
Lee <lees2bytes@gmail.com>
2013-05-01 16:52:51 -0400

    FEATURE: Extracted css from doc generation, can now be overridden with custom stylesheet.

--- src/main/java/com/meadowhawk/homepi/util/service/DocumentationRESTService.java
+++ src/main/java/com/meadowhawk/homepi/util/service/DocumentationRESTService.java
@@ -1,6 +1,7 @@
 package com.meadowhawk.homepi.util.service;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -9,15 +10,19 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import com.meadowhawk.homepi.service.UserRESTService;
+import com.meadowhawk.homepi.util.StringUtil;
 import com.meadowhawk.homepi.util.model.PublicRESTDoc;
 import com.meadowhawk.homepi.util.model.PublicRESTDocMethod;
 import com.meadowhawk.homepi.util.model.ServiceDocMethodTO;
@@ -31,24 +36,32 @@ import com.meadowhawk.homepi.util.model.ServiceDocTO;
 @PublicRESTDoc(serviceName = "DocumentationService", description = "Provides Documentation Services for all anotated REST EndPoints so the API can be easily documented from with in the codebase.")
 @Component
 public class DocumentationRESTService {
+	
 	private Log log = LogFactory.getLog(DocumentationRESTService.class);
 	private static final String CONTENT = "#Content#";
-//TODO: Add template loading or at least a CSS Param option for configuration.
+	private static final String DOC_APP_NAME = "#DOC_APP_NAME#";
+	private static final String STYLE_LINK = "<link rel=\"stylesheet\" type=\"text/css\" href=\"/services/docs/restDoc.css\">";
 	private static final String NO_CACHE = "<META HTTP-EQUIV=\"Pragma\" CONTENT=\"no-cache\"><META HTTP-EQUIV=\"Expires\" CONTENT=\"-0\">";
-	private static final String INDEX = "<HTML><head>" + NO_CACHE + "<style type=\"text/css\">a:link {text-decoration:none; color:#FFFFFF;} a:visited {text-decoration:none; color:#FFFFFF;} a:hover {text-decoration:underline;}</style></head><body><h2>WFM Service Endpoint Index.</h2><table>"+CONTENT+"</table></body></HTML>";
-
-	private static final String END_POINT = "<HTML><head>" + NO_CACHE + "<style type=\"text/css\">a:link {text-decoration:none; color:#06C} TH a:link {color:#FFFFFF;} a:visited {text-decoration:none; } TH a:visited{color:#FFFFFF;} a:hover {text-decoration:underline;} table.end-point{border-width: 1px; border-spacing: 0px; width: 100%} table.end-point th{vertical-align: top; text-align:left; width:120px} table.end-point td{text-align:left}</style></head><body><h2>INDEX_LINK>>WFM Service Endpoint Index.</h2><table>"+CONTENT+"</table></body></HTML>";
-	private static final String INDEX_LINK = "INDEX_LINK";
+	private static final String INDEX = "<HTML><head>" + NO_CACHE + STYLE_LINK +"</head><body><h2>"+ DOC_APP_NAME +" Service Endpoint Index.</h2><table>"+CONTENT+"</table></body></HTML>";
+	
+	private static final String END_POINT = "<HTML><head>" + NO_CACHE + STYLE_LINK + "</head><body><h2 class=\"breadcrumb\"><a href=\"../index\">Endpoint Index</a> >> "+ DOC_APP_NAME +" Service Endpoint Details.</h2><table>"+CONTENT+"</table></body></HTML>";
+	private static final String DEFAUT_STYLE = "a:link {text-decoration:none; color:#000} a:visited {text-decoration:none; color:#000} a:hover {text-decoration:underline;} TH a:link {color:#FFFFFF;} TH a:visited{text- decoration:none; color:#FFFFFF} TH a:hover {text-decoration:underline;} table.end-point{border-width: 1px; border-spacing: 0px; width: 100%} table.end-point th{vertical-align: top; text-align:left; width:120px} table.end-point td{text-align:left}";
 	
 	@Context UriInfo uriInfo;
+	
+	@Value("${apidocs.css.path:}")
+	private String altCssLocation;
 	
 	@Autowired
 	DocService docService;
 	
+	@Value("${apidocs.app_name}")
+	private String docAppName = "";
+	
 	@GET
 	@Path("/index")
 	@Produces({MediaType.TEXT_HTML})
-	@PublicRESTDocMethod(endPointName="Index", description="Generates an HTML page for browsing all supported EndPoints in the REST API.", sampleLinks={""})
+	@PublicRESTDocMethod(endPointName="Index", description="Generates an HTML page for browsing all supported EndPoints in the REST API.", sampleLinks={"./index"})
 	public String getRestServiceDocs() {
 		List<ServiceDocTO> serviceDocs = docService.getEndpointDocs();
 		StringBuilder indexString = new StringBuilder();
@@ -69,14 +82,14 @@ public class DocumentationRESTService {
 				.append("</TD></TR>").append("<TR><TD colspan=2>&nbsp;</TD></TR>");
 		}
 		
-		return INDEX.replaceFirst(CONTENT, indexString.toString());
+		return INDEX.replaceFirst(DOC_APP_NAME, docAppName).replaceFirst(CONTENT, indexString.toString());
 	}
 
 
 	@GET
 	@Path("/endpoint/{endPointName}")
 	@Produces({MediaType.TEXT_HTML,MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
-	@PublicRESTDocMethod(endPointName="EndPoint Documentation", description="Generates an HTML page for viewing the EndPoints supported by the requested service.", sampleLinks={"docs/endpoint/DocumentationService"}, errorCodes="404 - If bad service name is used.")
+	@PublicRESTDocMethod(endPointName="EndPoint Documentation", description="Generates an HTML page for viewing the EndPoints supported by the requested service.", sampleLinks={"./docs/endpoint/DocumentationService"}, errorCodes="404 - If bad service name is used.")
 	public String getRestServiceEndPointDocs(@PathParam("endPointName") String endPointName) {
 		StringBuilder indexString = new StringBuilder();
 		ServiceDocTO serviceDoc = docService.getEndpointDocsByName(endPointName);
@@ -110,9 +123,24 @@ public class DocumentationRESTService {
 				.append("<TR><TD colspan=2>&nbsp;</TD></TR></table>");
 		}
 		
-		return (END_POINT.replaceFirst(CONTENT, indexString.toString()).replaceFirst(INDEX_LINK, getIndexLinkHTML()));
+		return (END_POINT.replaceFirst(CONTENT, indexString.toString()).replaceFirst(DOC_APP_NAME, docAppName));
 	}
 
+	@GET
+	@Path("/restDoc.css")
+	@Produces("text/css")
+	@PublicRESTDocMethod(endPointName="StyleSheet override", description="Delivers the stylesheet used by the API docs. This can be overridden by including a new .css file in the WebApp folder and then setting the optional value in restDocs.properties,  pidocs.css.path=/{fileName}.css", sampleLinks={"./docs/restDoc.css"}, errorCodes="")
+	public Response getDefaultStyleSheet(){
+		if(!StringUtil.isNullOrEmpty(this.altCssLocation)){
+			try {
+				return Response.temporaryRedirect(uriInfo.getRequestUriBuilder().replacePath(this.altCssLocation).build()).build();
+			} catch (Exception e) {
+				return Response.ok(DEFAUT_STYLE).build();
+			}
+		}
+		return Response.ok(DEFAUT_STYLE).build();
+	}
+	
 	/**
 	 * @param values
 	 * @return
@@ -216,13 +244,5 @@ public class DocumentationRESTService {
 			log.debug("Trying to derive MethodPattern but failed, soemthing is probably wrong with the doc configuration. Method Name:" + methodName ,e);
 			return "";
 		}
-	}
-	
-	
-	private String getIndexLinkHTML() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("<a href=\"../index\">");
-		sb.append("Index</a>");
-		return sb.toString();
 	}
 }
