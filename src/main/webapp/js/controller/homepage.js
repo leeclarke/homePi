323ca4151c2644239879ff93cac39ed94cc36737
leeclarke <lees2bytes@gmail.com>
2013-07-23 22:10:48 -0400

    Partial conversion of home page to Angularjs

---
+++ src/main/webapp/js/controller/homepage.js
@@ -0,0 +1,15 @@
+HomePageCtrl
+/*Angluar controller*/
+angular.module('homepage', ['ngCookies']).
+  config(function($routeProvider) {
+    $routeProvider.
+      when('/', {controller:HomePageCtrl, templateUrl:'partials/home/home.html'}).
+      when('/getstarted', {controller:HomePageCtrl, templateUrl:'partials/home/getstarted.html'}).
+      when('/news', {controller:HomePageCtrl, templateUrl:'partials/home/news.html'}).
+	  when('/about', {controller:HomePageCtrl, templateUrl:'partials/home/about.html'}).
+      otherwise({redirectTo:'/'});
+  });
+
+function HomePageCtrl($scope, $http, $window, $location, $cookies) {
+
+}
